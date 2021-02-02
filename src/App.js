b65a21a6bdeb627af9692134e8dc8e7c0073a0f5
shams-aaliya <shamsaaliya08@gmail.com>
2021-02-02 22:45:29 +0530

    Day1 commit successful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import './App.css';
+import Header from './components/ui/Header'
+
 
 const App = () => {
   return (
     <div className="container">
-      Hello
+      <Header />
     </div>
   );
 }
