1498e9524c61238c55f1fdd2a39a733fe4adf015
shams-aaliya <shamsaaliya08@gmail.com>
2021-02-03 21:04:17 +0530

    Final commit Sucessfull

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
 
 const CharacterGrid = ({items, isLoading}) => {
-    return isLoading ? <h1>Loading...</h1> : <section className='cards'>
+    return isLoading ? <Spinner/> : <section className='cards'>
             {items.map(item => (
-                <h1>{item.name}</h1>
+                <CharacterItem key={item.char_id} item={item}></CharacterItem>
             ))}
         </section>
 }
